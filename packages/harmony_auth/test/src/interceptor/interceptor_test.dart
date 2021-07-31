de7f457dfa29de56bd43db1feb8a15b7bdb30b4c
FatulM <madani_amirreza@yahoo.com>
2021-07-31 15:53:06 +0430

    test(flutter): harmony_auth added more tests for interceptor

--- packages/harmony_auth/test/src/interceptor/interceptor_test.dart
+++ packages/harmony_auth/test/src/interceptor/interceptor_test.dart
@@ -15,6 +15,10 @@ import 'package:mocktail/mocktail.dart';
 import '../../utils/logger.dart';
 import '../../utils/storage.dart';
 
+const keyRetry = 'harmony_auth_is_retry';
+const testUrl = 'https://test';
+const refreshUrl = 'https://refresh';
+
 class MockAdapter extends Mock implements HttpClientAdapter {}
 
 class FakeRequestOptions extends Fake implements RequestOptions {}
@@ -39,7 +43,7 @@ void main() {
           matcher: AuthMatcher.none(),
           rest: AuthRestImpl(
             dio: dio,
-            refreshUrl: 'https://refresh',
+            refreshUrl: refreshUrl,
             logger: noopLogger,
           ),
           logger: noopLogger,
@@ -57,6 +61,7 @@ void main() {
         when(() => adapter.fetch(
               any(
                 that: predicate((RequestOptions request) =>
+                    request.path == testUrl &&
                     request.headers['authorization'] == null),
               ),
               any(),
@@ -66,8 +71,232 @@ void main() {
               200,
             ));
 
-        await dio.get<dynamic>('https://test');
+        await dio.get<dynamic>(testUrl);
       });
+    });
+
+    group('matching call', () {
+      setUp(() {
+        registerFallbackValue(FakeRequestOptions());
+        adapter = MockAdapter();
+        when(() => adapter.close()).thenAnswer((_) {});
+        storage = InMemoryAuthStorage();
+        dio = Dio();
+        dio.httpClientAdapter = adapter;
+        dio.interceptors.add(AuthInterceptor(
+          dio: dio,
+          storage: storage,
+          matcher: AuthMatcher.all(),
+          rest: AuthRestImpl(
+            dio: dio,
+            refreshUrl: refreshUrl,
+            logger: noopLogger,
+          ),
+          logger: noopLogger,
+        ));
+      });
+
+      tearDown(() {
+        resetMocktailState();
+      });
+
+      test(
+        'access -> success',
+        () async {
+          await storage.setAccessToken('a1');
+          await storage.setRefreshToken('r1');
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == testUrl &&
+                      request.headers['authorization'] == 'bearer a1' &&
+                      request.extra[keyRetry] == null),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => ResponseBody(
+                Stream.empty(),
+                200,
+              ));
+
+          await dio.get<dynamic>(testUrl);
+
+          expect(await storage.getAccessToken(), equals('a1'));
+          expect(await storage.getRefreshToken(), equals('r1'));
+        },
+      );
+
+      test(
+        'access -> network fail',
+        () async {
+          await storage.setAccessToken('a1');
+          await storage.setRefreshToken('r1');
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == testUrl &&
+                      request.headers['authorization'] == 'bearer a1' &&
+                      request.extra[keyRetry] == null),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => throw SocketException('error'));
+
+          expect(
+            () async {
+              try {
+                await dio.get<dynamic>(testUrl);
+              } finally {
+                expect(await storage.getAccessToken(), equals('a1'));
+                expect(await storage.getRefreshToken(), equals('r1'));
+              }
+            },
+            throwsA(
+              predicate((DioError e) =>
+                  e.type == DioErrorType.other && e.error is SocketException),
+            ),
+          );
+        },
+      );
+
+      test(
+        'access -> auth fail -> (refresh -> success) -> retry -> success',
+        () async {
+          await storage.setAccessToken('a1');
+          await storage.setRefreshToken('r1');
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == testUrl &&
+                      request.headers['authorization'] == 'bearer a1' &&
+                      request.extra[keyRetry] == null),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => ResponseBody(
+                Stream.empty(),
+                401,
+              ));
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == refreshUrl &&
+                      request.headers['authorization'] == null &&
+                      request.data['refresh'] == 'r1'),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => ResponseBody(
+                Stream.fromIterable([
+                  Uint8List.fromList(
+                    utf8.encode(
+                      '{"refresh":"r2", "access":"a2"}',
+                    ),
+                  ),
+                ]),
+                200,
+                headers: {
+                  'content-type': ['application/json'],
+                  'accept': ['application/json'],
+                },
+              ));
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == testUrl &&
+                      request.headers['authorization'] == 'bearer a2' &&
+                      request.extra[keyRetry] != null),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => ResponseBody(
+                Stream.empty(),
+                200,
+              ));
+
+          await dio.get<dynamic>(testUrl);
+
+          expect(await storage.getAccessToken(), equals('a2'));
+          expect(await storage.getRefreshToken(), equals('r2'));
+        },
+      );
+
+      test(
+        'access -> auth fail -> (refresh -> success) -> retry -> fail network',
+        () async {
+          await storage.setAccessToken('a1');
+          await storage.setRefreshToken('r1');
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == testUrl &&
+                      request.headers['authorization'] == 'bearer a1' &&
+                      request.extra[keyRetry] == null),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => ResponseBody(
+                Stream.empty(),
+                401,
+              ));
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == refreshUrl &&
+                      request.headers['authorization'] == null &&
+                      request.data['refresh'] == 'r1'),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => ResponseBody(
+                Stream.fromIterable([
+                  Uint8List.fromList(
+                    utf8.encode(
+                      '{"refresh":"r2", "access":"a2"}',
+                    ),
+                  ),
+                ]),
+                200,
+                headers: {
+                  'content-type': ['application/json'],
+                  'accept': ['application/json'],
+                },
+              ));
+
+          when(() => adapter.fetch(
+                any(
+                  that: predicate((RequestOptions request) =>
+                      request.path == testUrl &&
+                      request.headers['authorization'] == 'bearer a2' &&
+                      request.extra[keyRetry] != null),
+                ),
+                any(),
+                any(),
+              )).thenAnswer((_) async => throw SocketException('error'));
+
+          expect(
+            () async {
+              try {
+                await dio.get<dynamic>(testUrl);
+              } finally {
+                expect(await storage.getAccessToken(), equals('a2'));
+                expect(await storage.getRefreshToken(), equals('r2'));
+              }
+            },
+            throwsA(
+              predicate((DioError e) =>
+                  e.type == DioErrorType.other && e.error is SocketException),
+            ),
+          );
+        },
+      );
     });
   });
 }
