28b93ce8115ff8083ee3c2a12240d9111ab46c3c
FatulM <madani_amirreza@yahoo.com>
2021-08-04 16:15:00 +0430

    test(harmony_auth): added test for access only rest impl

--- packages/harmony_auth/test/src/rest/rest_test.dart
+++ packages/harmony_auth/test/src/rest/rest_test.dart
@@ -207,5 +207,187 @@ void main() {
         expect(matcher.matches('POST', testUrl), isFalse);
       });
     });
+
+    group('accessOnly', () {
+      group('method refreshTokens', () {
+        late MockAdapter adapter;
+        late AuthRest rest;
+
+        setUp(() {
+          registerFallbackValue(FakeRequestOptions());
+          adapter = MockAdapter();
+          when(() => adapter.close()).thenAnswer((_) {});
+          rest = AuthRest.accessOnly(
+            dio: Dio()..httpClientAdapter = adapter,
+            refreshUrl: refreshUrl,
+          );
+        });
+
+        tearDown(() {
+          resetMocktailState();
+        });
+
+        test(
+          'when response is ok then should return tokens',
+          () async {
+            when(() => adapter.fetch(
+                  any(
+                    that: predicate((RequestOptions request) =>
+                        request.path == refreshUrl &&
+                        request.method == 'POST' &&
+                        request.headers['content-type'] == 'application/json' &&
+                        request.headers['accept'] == 'application/json' &&
+                        request.data['refresh'] == 'r1'),
+                  ),
+                  any(),
+                  any(),
+                )).thenAnswer((_) async => ResponseBody(
+                  Stream.fromIterable([
+                    Uint8List.fromList(
+                      utf8.encode(
+                        '{"access":"a2"}',
+                      ),
+                    ),
+                  ]),
+                  200,
+                  headers: {
+                    'content-type': ['application/json'],
+                    'accept': ['application/json'],
+                  },
+                ));
+
+            final pair = await rest.refreshTokens('r1');
+            expect(pair.refresh, equals('r1'));
+            expect(pair.access, equals('a2'));
+          },
+        );
+
+        test(
+          'when is unauthorized then should error with AuthException',
+          () async {
+            when(() => adapter.fetch(
+                  any(
+                    that: predicate(
+                      (RequestOptions request) =>
+                          request.path == refreshUrl &&
+                          request.method == 'POST' &&
+                          request.headers['content-type'] ==
+                              'application/json' &&
+                          request.headers['accept'] == 'application/json' &&
+                          request.data['refresh'] == 'r1',
+                    ),
+                  ),
+                  any(),
+                  any(),
+                )).thenAnswer((_) async => ResponseBody(
+                  Stream.empty(),
+                  401,
+                  headers: {
+                    'content-type': ['application/json'],
+                    'accept': ['application/json'],
+                  },
+                ));
+
+            expect(
+              () async => await rest.refreshTokens('r1'),
+              throwsA(
+                predicate(
+                  (DioError e) =>
+                      e.type == DioErrorType.other &&
+                      e.error is AuthException &&
+                      e.requestOptions.path == refreshUrl,
+                ),
+              ),
+            );
+          },
+        );
+
+        test(
+          'when has bad response then should error with AssertionError',
+          () async {
+            when(() => adapter.fetch(
+                  any(
+                    that: predicate(
+                      (RequestOptions request) =>
+                          request.path == refreshUrl &&
+                          request.method == 'POST' &&
+                          request.headers['content-type'] ==
+                              'application/json' &&
+                          request.headers['accept'] == 'application/json' &&
+                          request.data['refresh'] == 'r1',
+                    ),
+                  ),
+                  any(),
+                  any(),
+                )).thenAnswer((_) async => ResponseBody(
+                  Stream.fromIterable([
+                    Uint8List.fromList(
+                      utf8.encode(
+                        '{"hello": "hi"}',
+                      ),
+                    ),
+                  ]),
+                  200,
+                  headers: {
+                    'content-type': ['application/json'],
+                    'accept': ['application/json'],
+                  },
+                ));
+
+            expect(
+              () async => await rest.refreshTokens('r1'),
+              throwsA(
+                predicate(
+                  (DioError e) =>
+                      e.type == DioErrorType.other && e.error is AssertionError,
+                ),
+              ),
+            );
+          },
+        );
+
+        test(
+          'when with network error then should error with SocketException',
+          () async {
+            when(() => adapter.fetch(
+                  any(
+                    that: predicate(
+                      (RequestOptions request) =>
+                          request.path == refreshUrl &&
+                          request.method == 'POST' &&
+                          request.headers['content-type'] ==
+                              'application/json' &&
+                          request.headers['accept'] == 'application/json' &&
+                          request.data['refresh'] == 'r1',
+                    ),
+                  ),
+                  any(),
+                  any(),
+                )).thenAnswer((_) async => throw SocketException('error'));
+
+            expect(
+              () async => await rest.refreshTokens('r1'),
+              throwsA(
+                predicate((DioError e) =>
+                    e.type == DioErrorType.other && e.error is SocketException),
+              ),
+            );
+          },
+        );
+      });
+
+      test('getter refreshTokensMatcher', () {
+        final rest = AuthRest.accessOnly(
+          dio: Dio(),
+          refreshUrl: refreshUrl,
+        );
+
+        final matcher = rest.refreshTokensMatcher;
+
+        expect(matcher.matches('POST', refreshUrl), isTrue);
+        expect(matcher.matches('GET', refreshUrl), isFalse);
+        expect(matcher.matches('POST', testUrl), isFalse);
+      });
+    });
   });
 }
