7d30c18c7e86c04523d273f5e550f95f8eae1955
FatulM <madani_amirreza@yahoo.com>
2021-08-16 16:44:19 +0430

    feat(harmony_log): added log class bones

--- packages/harmony_log/lib/src/level/level.dart
+++ packages/harmony_log/lib/src/level/level.dart
@@ -1,10 +1,25 @@
 /// log level
+///
+/// Level with more significance is bigger
 enum LogLevel {
+  /// significance: 0
   verbose,
+
+  /// significance: 1
   debug,
+
+  /// significance: 2
   info,
+
+  /// significance: 3
   warning,
+
+  /// significance: 4
   error,
+
+  /// significance: 5
+  ///
+  /// (what a terrible failure)
   wtf,
 }
 
--- packages/harmony_log/lib/src/log/log.dart
+++ packages/harmony_log/lib/src/log/log.dart
@@ -1,1 +1,83 @@
+import 'package:harmony_log/src/level/level.dart';
 
+/// Log class
+///
+/// logger has null tag by default
+abstract class Log {
+  void init();
+
+  /// get this logger's tag
+  ///
+  /// logger has null tag by default
+  String? get tag;
+
+  /// create a new logger based on this logger with a new tag
+  Log withTag(String? tag);
+
+  /// log
+  ///
+  /// use abbreviated methods like: [v], ..., [wtf]
+  void log({
+    LogLevel level,
+    String message,
+    Object? error,
+    StackTrace? stackTrace,
+    Object? extra,
+  });
+
+  /// verbose
+  void v(
+    String message, [
+    Object? error,
+    StackTrace? stackTrace,
+    Object? extra,
+  ]);
+
+  /// debug
+  void d(
+    String message, [
+    Object? error,
+    StackTrace? stackTrace,
+    Object? extra,
+  ]);
+
+  /// info
+  void i(
+    String message, [
+    Object? error,
+    StackTrace? stackTrace,
+    Object? extra,
+  ]);
+
+  /// warning
+  void w(
+    String message, [
+    Object? error,
+    StackTrace? stackTrace,
+    Object? extra,
+  ]);
+
+  /// error
+  void e(
+    String message, [
+    Object? error,
+    StackTrace? stackTrace,
+    Object? extra,
+  ]);
+
+  /// wtf
+  /// (what a terrible failure)
+  void wtf(
+    String message, [
+    Object? error,
+    StackTrace? stackTrace,
+    Object? extra,
+  ]);
+
+  /// close
+  ///
+  /// after closing you can not call log methods
+  ///
+  /// all of tagged loggers also closes
+  void close();
+}
