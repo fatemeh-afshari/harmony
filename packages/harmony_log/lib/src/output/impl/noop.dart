90d29b72aa8d3301e6356d4203d8543a3ad28ec3
FatulM <madani_amirreza@yahoo.com>
2021-08-28 13:11:53 +0430

    refactor(harmony_log): refactor test structure

---
+++ packages/harmony_log/lib/src/output/impl/noop.dart
@@ -0,0 +1,21 @@
+import 'package:harmony_log/src/event/event.dart';
+import 'package:harmony_log/src/output/output.dart';
+
+class OutputMultiImpl implements LogOutput {
+  const OutputMultiImpl();
+
+  @override
+  void close() {
+    // TODO: implement close
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
+
+  @override
+  void write(LogEvent event) {
+    // TODO: implement write
+  }
+}
---
+++ packages/harmony_log/test/src/log/impl/tagged_test.dart
@@ -0,0 +1,119 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:harmony_log/src/event/event.dart';
+import 'package:harmony_log/src/level/level.dart';
+import 'package:harmony_log/src/log/log.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockLog extends Mock implements Log {}
+
+class FakeLogEvent extends Fake implements LogEvent {}
+
+void main() {
+  group('Log', () {
+    group('tagged', () {
+      late MockLog base;
+      late Log log;
+
+      setUp(() {
+        base = MockLog();
+        log = Log.tagged(tag: 'TAG', child: base);
+      });
+
+      tearDown(() {
+        resetMocktailState();
+      });
+
+      test('init', () {
+        log.init();
+        verify(() => base.init()).called(1);
+      });
+
+      test('close', () {
+        log.close();
+        verify(() => base.close()).called(1);
+      });
+
+      test('tag', () {
+        expect(log.tag, equals('TAG'));
+        verifyNever(() => base.tag);
+      });
+
+      test('write', () {
+        final event = FakeLogEvent();
+        log.write(event);
+        verify(() => base.write(event)).called(1);
+      });
+
+      test('log', () {
+        final trace = StackTrace.empty;
+        log.log(
+          LogLevel.warning,
+          'message',
+          error: 'error',
+          stackTrace: trace,
+          extra: 'extra',
+        );
+        verify(() => base.log(
+              LogLevel.warning,
+              'message',
+              error: 'error',
+              stackTrace: trace,
+              extra: 'extra',
+            )).called(1);
+      });
+    });
+
+    group('LogTagExt', () {
+      late MockLog base;
+      late Log log;
+
+      setUp(() {
+        base = MockLog();
+        log = base.tagged('TAG');
+      });
+
+      tearDown(() {
+        resetMocktailState();
+      });
+
+      test('init', () {
+        log.init();
+        verify(() => base.init()).called(1);
+      });
+
+      test('close', () {
+        log.close();
+        verify(() => base.close()).called(1);
+      });
+
+      test('tag', () {
+        expect(log.tag, equals('TAG'));
+        verifyNever(() => base.tag);
+      });
+
+      test('write', () {
+        final event = FakeLogEvent();
+        log.write(event);
+        verify(() => base.write(event)).called(1);
+      });
+
+      test('log', () {
+        final trace = StackTrace.empty;
+        log.log(
+          LogLevel.warning,
+          'message',
+          error: 'error',
+          stackTrace: trace,
+          extra: 'extra',
+        );
+        verify(() => base.log(
+              LogLevel.warning,
+              'message',
+              error: 'error',
+              stackTrace: trace,
+              extra: 'extra',
+            )).called(1);
+      });
+    });
+  });
+}
