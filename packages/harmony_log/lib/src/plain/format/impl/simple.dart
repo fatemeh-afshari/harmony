42af8a9d1e23a17c65510381253658e1f3a360fc
FatulM <madani_amirreza@yahoo.com>
2021-08-29 19:46:22 +0430

    feat(harmony_log): implemented simple plain output

--- packages/harmony_log/lib/src/plain/format/impl/simple.dart
+++ packages/harmony_log/lib/src/plain/format/impl/simple.dart
@@ -1,15 +1,28 @@
 import 'package:harmony_log/src/event/event.dart';
+import 'package:harmony_log/src/level/level.dart';
 import 'package:harmony_log/src/plain/format/format.dart';
 
 class LogPlainFormatSimpleImpl implements LogPlainFormat {
+  static const prefix = 'HARMONY';
+
   const LogPlainFormatSimpleImpl();
 
   @override
   Iterable<String> format(LogEvent event) sync* {
-    yield event.level.toString().substring('LogLevel.'.length) +
-        ' ' +
-        (event.tag ?? 'DEFAULT') +
-        ' ' +
-        event.message;
+    yield _line(event);
   }
+
+  String _line(LogEvent event) {
+    final s = StringBuffer();
+    if (prefix.isNotEmpty) s.write('$prefix ');
+    s.write('${_level(event.level)} ');
+    if (event.tag != null) s.write('${event.tag} ');
+    s.write('${event.message} ');
+    if (event.error != null) s.write('${event.error} ');
+    if (event.stackTrace != null) s.write('${event.stackTrace} ');
+    return s.toString();
+  }
+
+  String _level(LogLevel level) =>
+      level.toString().substring('LogEvent'.length);
 }
