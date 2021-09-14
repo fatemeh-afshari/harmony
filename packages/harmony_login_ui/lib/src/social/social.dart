478a6f89d9bdaea417c3ec3415abd293d09dd645
FatulM <madani_amirreza@yahoo.com>
2021-09-14 16:13:53 +0430

    feat(harmony_login_ui): fix social ui button implementation

--- packages/harmony_login_ui/lib/src/social/social.dart
+++ packages/harmony_login_ui/lib/src/social/social.dart
@@ -3,8 +3,8 @@ import 'package:harmony_auth/harmony_auth.dart';
 import 'package:harmony_fire/harmony_fire.dart';
 import 'package:harmony_login/harmony_login.dart';
 
-class LoginUISocial extends StatelessWidget {
-  final String name;
+class LoginUISocial extends StatefulWidget {
+  final String provider;
   final String title;
   final String icon;
 
@@ -12,28 +12,25 @@ class LoginUISocial extends StatelessWidget {
   final LoginSystem loginSystem;
   final FireSigning fireSigning;
 
-  final void Function(String email, String? name) onSuccess;
-  final void Function() onCancelled;
-  final void Function(Object error) onError;
+  final void Function(String email, String? provider) onSuccess;
 
   const LoginUISocial.apple({
     Key? key,
     required AuthRepository authRepository,
     required LoginSystem loginSystem,
     required FireSigning fireSigning,
-    required void Function(String email, String? name) onSuccess,
+    required void Function(String email, String? provider) onSuccess,
     required void Function() onCancelled,
     required void Function(Object error) onError,
   }) : this._internal(
-          name: 'apple',
+          key: key,
+          provider: 'apple',
           title: 'Login with Apple',
           icon: 'asset/image/apple.png',
           authRepository: authRepository,
           loginSystem: loginSystem,
           fireSigning: fireSigning,
           onSuccess: onSuccess,
-          onCancelled: onCancelled,
-          onError: onError,
         );
 
   const LoginUISocial.facebook({
@@ -41,19 +38,18 @@ class LoginUISocial extends StatelessWidget {
     required AuthRepository authRepository,
     required LoginSystem loginSystem,
     required FireSigning fireSigning,
-    required void Function(String email, String? name) onSuccess,
+    required void Function(String email, String? provider) onSuccess,
     required void Function() onCancelled,
     required void Function(Object error) onError,
   }) : this._internal(
-          name: 'facebook',
+          key: key,
+          provider: 'facebook',
           title: 'Login with Facebook',
           icon: 'asset/image/facebook.png',
           authRepository: authRepository,
           loginSystem: loginSystem,
           fireSigning: fireSigning,
           onSuccess: onSuccess,
-          onCancelled: onCancelled,
-          onError: onError,
         );
 
   const LoginUISocial.google({
@@ -61,41 +57,45 @@ class LoginUISocial extends StatelessWidget {
     required AuthRepository authRepository,
     required LoginSystem loginSystem,
     required FireSigning fireSigning,
-    required void Function(String email, String? name) onSuccess,
+    required void Function(String email, String? provider) onSuccess,
     required void Function() onCancelled,
     required void Function(Object error) onError,
   }) : this._internal(
-          name: 'google',
+          key: key,
+          provider: 'google',
           title: 'Login with Google',
           icon: 'asset/image/google.png',
           authRepository: authRepository,
           loginSystem: loginSystem,
           fireSigning: fireSigning,
           onSuccess: onSuccess,
-          onCancelled: onCancelled,
-          onError: onError,
         );
 
   const LoginUISocial._internal({
     Key? key,
-    required this.name,
+    required this.provider,
     required this.title,
     required this.icon,
     required this.authRepository,
     required this.loginSystem,
     required this.fireSigning,
     required this.onSuccess,
-    required this.onCancelled,
-    required this.onError,
   }) : super(key: key);
+
+  @override
+  _LoginUISocialState createState() => _LoginUISocialState();
+}
+
+class _LoginUISocialState extends State<LoginUISocial> {
+  var _loading = false;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
-      onPressed: _onPressed,
-      label: Text(title),
+      onPressed: _loading ? null : _onPressed,
+      label: Text(widget.title),
       icon: Image(
-        image: AssetImage(icon),
+        image: AssetImage(widget.icon),
         width: 32,
         height: 32,
       ),
@@ -103,30 +103,35 @@ class LoginUISocial extends StatelessWidget {
   }
 
   Future<void> _onPressed() async {
+    setState(() => _loading = true);
     try {
-      final provider = FireProvider.of(name);
-      final signingInfo = await fireSigning.signInUpSocial(provider);
+      final provider = FireProvider.of(widget.provider);
+      final signingInfo = await widget.fireSigning.signInUpSocial(provider);
       try {
-        final social = loginSystem.social();
+        final social = widget.loginSystem.social();
         final result = await social.login(
           signingInfo.provider,
           signingInfo.email,
         );
-        await authRepository.setToken(AuthToken(
+        await widget.authRepository.setToken(AuthToken(
           refresh: result.refresh,
           access: result.access,
         ));
-        onSuccess(signingInfo.email, signingInfo.displayName);
+        widget.onSuccess(signingInfo.email, signingInfo.displayName);
       } catch (_) {
         try {
-          await fireSigning.signOut();
+          await widget.fireSigning.signOut();
         } catch (__) {}
         rethrow;
       }
     } on FireCancelledException {
-      onCancelled();
-    } catch (e) {
-      onError(e);
+      // nothing
+    } catch (_) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('A problem occurred')),
+      );
+    } finally {
+      setState(() => _loading = false);
     }
   }
 }
