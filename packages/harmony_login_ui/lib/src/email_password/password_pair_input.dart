1cfaf394992ced56c66d2a3d0ed357e2bbeab78f
FatulM <madani_amirreza@yahoo.com>
2021-09-14 17:41:45 +0430

    feat(harmony_login_ui): added password pair input

---
+++ packages/harmony_login_ui/lib/src/email_password/password_pair_input.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class _PasswordPair {
+  final String pass;
+  final String confirm;
+  final bool isObscure;
+
+  const _PasswordPair(this.pass, this.confirm, this.isObscure);
+
+  const _PasswordPair.empty(bool isObscure) : this('', '', isObscure);
+
+  @override
+  String toString() => '_PasswordPair{pass: $pass, confirm: $confirm}';
+}
+
+class PasswordPairFromField extends FormField<_PasswordPair> {
+  PasswordPairFromField({
+    Key? key,
+    String? Function(String pass)? validator,
+    void Function(String pass)? onSaved,
+    String? passwordHint,
+    String? confirmHint,
+    String? matchErrorMessage,
+    bool showObscureIcon = true,
+  }) : super(
+          key: key,
+          validator: (_PasswordPair? value) {
+            final pair = value!;
+            final pass = pair.pass;
+            final confirm = pair.confirm;
+            final base = validator?.call(pass);
+            return base ?? (pass == confirm ? null : matchErrorMessage);
+          },
+          onSaved: (_PasswordPair? value) {
+            final pair = value!;
+            final pass = pair.pass;
+            onSaved?.call(pass);
+          },
+          initialValue: _PasswordPair.empty(showObscureIcon),
+          builder: (field) {
+            final error = field.errorText;
+            final pair = field.value!;
+            final pass = pair.pass;
+            final confirm = pair.confirm;
+            final isObscure = pair.isObscure;
+            return Column(
+              children: [
+                TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: isObscure,
+                  decoration: InputDecoration(
+                    hintText: passwordHint,
+                    suffixIcon: showObscureIcon
+                        ? GestureDetector(
+                            onTap: () {
+                              field.didChange(_PasswordPair(
+                                pass,
+                                confirm,
+                                !isObscure,
+                              ));
+                            },
+                            child: isObscure
+                                ? const Icon(Icons.visibility)
+                                : const Icon(Icons.visibility_off),
+                          )
+                        : null,
+                    errorText: (error != null && error != matchErrorMessage)
+                        ? error
+                        : null,
+                  ),
+                  onChanged: (String value) {
+                    field.didChange(
+                        _PasswordPair(value.trim(), confirm, isObscure));
+                  },
+                ),
+                const SizedBox(height: 32),
+                TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: isObscure,
+                  decoration: InputDecoration(
+                    hintText: confirmHint,
+                    errorText: (error != null && error == matchErrorMessage)
+                        ? matchErrorMessage
+                        : null,
+                  ),
+                  onChanged: (String value) {
+                    field.didChange(
+                        _PasswordPair(pass, value.trim(), isObscure));
+                  },
+                ),
+              ],
+            );
+          },
+        );
+}
