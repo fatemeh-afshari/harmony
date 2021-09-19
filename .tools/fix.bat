aca95c9c8b0c40d7418212164e5c35b1e99074dc
FatulM <madani_amirreza@yahoo.com>
2021-09-19 15:05:43 +0430

    ci(all): added fix tools

---
+++ .tools/fix.bat
@@ -0,0 +1,32 @@
+@echo off
+
+echo HEADER init
+
+pushd packages || exit 1
+
+pushd harmony_log || exit 1
+echo NOTE harmony_log
+call dart format --fix lib test guide
+popd || exit 1
+
+pushd harmony_auth || exit 1
+echo NOTE harmony_auth
+call dart format --fix lib test guide
+popd || exit 1
+
+pushd harmony_fire || exit 1
+echo NOTE harmony_fire
+call dart format --fix lib test
+popd || exit 1
+
+pushd harmony_login || exit 1
+echo NOTE harmony_login
+call dart format --fix lib test
+popd || exit 1
+
+pushd harmony_login_ui || exit 1
+echo NOTE harmony_login_ui
+call dart format --fix lib test
+popd || exit 1
+
+popd || exit 1
