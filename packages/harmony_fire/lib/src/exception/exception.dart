51e129b744e918f0373c453896c6ab923a111853
FatulM <madani_amirreza@yahoo.com>
2021-09-21 16:00:54 +0430

    feat(harmony_fire): used harmony_log, refactor logging

--- packages/harmony_fire/lib/src/exception/exception.dart
+++ packages/harmony_fire/lib/src/exception/exception.dart
@@ -1,8 +1,11 @@
+import 'package:meta/meta.dart';
+
 /// harmony_fire operation cancelled.
 ///
 /// other types of exceptions are redirected exceptions
 /// of firebase.
 class FireCancelledException implements Exception {
+  @internal
   const FireCancelledException();
 
   @override
