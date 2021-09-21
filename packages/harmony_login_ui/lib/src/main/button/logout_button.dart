f77be7311df2eefe64622129943418eadd82036d
FatulM <madani_amirreza@yahoo.com>
2021-09-21 18:03:49 +0430

    feat(harmony_login_ui): sign out of fire on logout

--- packages/harmony_login_ui/lib/src/main/button/logout_button.dart
+++ packages/harmony_login_ui/lib/src/main/button/logout_button.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:harmony_auth/harmony_auth.dart';
+import 'package:harmony_fire/harmony_fire.dart';
 import 'package:harmony_login/harmony_login.dart';
 import 'package:harmony_login_ui/src/widgets/loading_text_button.dart';
 
 class LogoutButton extends StatefulWidget {
   final AuthRepository authRepository;
   final LoginSystem loginSystem;
+  final FireSigning? fireSigning;
 
   final void Function() onSuccess;
 
@@ -13,6 +15,7 @@ class LogoutButton extends StatefulWidget {
     Key? key,
     required this.authRepository,
     required this.loginSystem,
+    this.fireSigning,
     required this.onSuccess,
   }) : super(key: key);
 
@@ -37,6 +40,7 @@ class _LogoutButtonState extends State<LogoutButton> {
     setState(() => _loading = true);
     try {
       await widget.loginSystem.logout();
+      await widget.fireSigning?.signOut();
       await widget.authRepository.removeToken();
       widget.onSuccess();
     } catch (_) {
